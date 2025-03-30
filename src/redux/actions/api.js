4d4ceb274ac926d30203323cf0abefbdeb5ad483
Shailedra-RapidQuest <shailendratrivedi009@gmail.com>
2025-03-30 13:45:48 +0530

    Udpate the baseURL

--- src/redux/actions/api.js
+++ src/redux/actions/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
-  // baseURL: "https://ems-nodejs-sh9d.onrender.com",
+  // baseURL: "http://localhost:8080",
+  baseURL: "https://ems-nodejs-sh9d.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
